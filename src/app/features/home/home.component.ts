c393e16297f9fcb168a6acb48302ffca889c4b24
a.gamal <a.gamal@palmerasys.com>
2025-04-07 04:36:51 +0200

    up

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -4,10 +4,11 @@ import { CarouselComponent } from "./carousel/carousel.component";
 import { HomeDetailComponent } from "./home-detail/home-detail.component";
 import { FooterComponent } from "./footer/footer.component";
 import { CommonModule } from '@angular/common';
+import { HeaderCategoriesComponent } from "./header-categories/header-categories.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, HeaderComponent, CarouselComponent, HomeDetailComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, CarouselComponent, HomeDetailComponent, FooterComponent, HeaderCategoriesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
